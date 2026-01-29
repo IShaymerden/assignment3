766a821099e8749a64a08db598c1a316617e3c49
Iskander <bebrakebaba@gmail.com>
2026-01-29 11:49:12 +0500

    ф

--- myservice/src/main/java/kz/aitu/myservice/entities/Passenger.java
+++ myservice/src/main/java/kz/aitu/myservice/entities/Passenger.java
@@ -7,21 +7,21 @@ import jakarta.persistence.*;
 public class Passenger {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
     private String passportNumber;
 
     public Passenger() {
 
     }
-    public Passenger(int id, String name, String passportNumber) {
+    public Passenger(Integer id, String name, String passportNumber) {
         this.id = id;
         this.name = name;
         this.passportNumber = passportNumber;
     }
 
 
-    public int getId() { return id; }
+    public Integer getId() { return id; }
     public void setId(int id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
--- myservice/src/main/java/kz/aitu/myservice/repositories/PassengerRepository.java
+++ myservice/src/main/java/kz/aitu/myservice/repositories/PassengerRepository.java
@@ -1,8 +1,9 @@
 package kz.aitu.myservice.repositories;
 
-import kz.aitu.myservice.entities.Passenger; // проверь, чтобы путь к твоей сущности был верным
+import kz.aitu.myservice.entities.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface PassengerRepository extends JpaRepository<Passenger, Integer> {
